91d39f86f10f3a0014bbf82faa8f717b42966e96
Giuseppe Grimaldi <grimaldi.giuseppe@outlook.com>
2026-09-07 10:18:10 +0200

    chore:inizializzazione progetto MedSupply-Hub

---
+++ src/main/java/com/medsupplyhub/medsupply_hub/MedsupplyHubApplication.java
@@ -0,0 +1,13 @@
+package com.medsupplyhub.medsupply_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsupplyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsupplyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medsupplyhub/medsupply_hub/MedsupplyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medsupplyhub.medsupply_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsupplyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
