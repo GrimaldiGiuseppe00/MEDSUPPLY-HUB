51b223a8a19d497f2dbf094dedd4f39f4a7680d4
Giuseppe Grimaldi <grimaldi.giuseppe@outlook.com>
2026-09-09 19:34:26 +0200

    feat:implementato logging nella creazione cliente

--- src/main/java/com/medsupplyhub/medsupply_hub/service/impl/UtenteServiceImpl.java
+++ src/main/java/com/medsupplyhub/medsupply_hub/service/impl/UtenteServiceImpl.java
@@ -2,6 +2,8 @@ package com.medsupplyhub.medsupply_hub.service.impl;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +21,6 @@ import com.medsupplyhub.medsupply_hub.model.Utente;
 import com.medsupplyhub.medsupply_hub.repository.ClienteRepository;
 import com.medsupplyhub.medsupply_hub.repository.UtenteRepository;
 import com.medsupplyhub.medsupply_hub.service.UtenteService;
-
-import jakarta.transaction.Transactional;
 @Service
 public class UtenteServiceImpl implements UtenteService{
 	
@@ -28,7 +28,7 @@ public class UtenteServiceImpl implements UtenteService{
 	private final UtenteMapper utenteMapper;
 	private final ClienteRepository clienteRepository;
 	private final PasswordEncoder passwordEncoder;
-	
+	final static Logger logger = LoggerFactory.getLogger(UtenteServiceImpl.class);
 
 	public UtenteServiceImpl(UtenteRepository utenteRepository, UtenteMapper utenteMapper,ClienteRepository clienteRepository,PasswordEncoder passwordEncoder) {
 		this.utenteRepository = utenteRepository;
@@ -36,49 +36,72 @@ public class UtenteServiceImpl implements UtenteService{
 		this.clienteRepository = clienteRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
-	@Transactional
 	@Override
+	@org.springframework.transaction.annotation.Transactional
 	public UtenteResponseDto creazioneUtenteDaAdmin(
 	        CreazioneUtenteDto utenteDaUi) {
-
+		 logger.info(
+		            "Avvio creazione utente con ruolo={}",
+		            utenteDaUi.ruoloUtente()
+		    );
 	    // verifica email già esistente
 	    if (utenteRepository.existsByEmail(utenteDaUi.email())) {
+	    	 logger.warn(
+	                 "Creazione utente rifiutata: email già esistente"
+	         );
 	        throw new EmailGiaEsistenteException(utenteDaUi.email());
 	    }
 
 	    // se NON è CLIENTE, non deve avere clienteId
 	    if (utenteDaUi.ruoloUtente() != RuoloUtente.CLIENTE
 	            && utenteDaUi.clienteId() != null) {
-
+	    	logger.warn(
+                    "Creazione utente interno MedSupply rifiutata: clienteId non ammesso"
+            );
 	        throw new ClienteNonAmmessoException();
 	    }
 
 	    // DTO -> Entity
 	    Utente utente =
 	            utenteMapper.creazioneUtenteDtoToUtente(utenteDaUi);
-
+	    
 	    // se è CLIENTE deve avere un Cliente associato
 	    if (utenteDaUi.ruoloUtente() == RuoloUtente.CLIENTE) {
 
 	        if (utenteDaUi.clienteId() == null) {
+	        	logger.warn(
+	                    "Creazione utente CLIENTE rifiutata: clienteId mancante"
+	            );
 	            throw new ClienteObbligatorioException();
 	        }
 
 	        Cliente clienteDaDb = clienteRepository
 	                .findById(utenteDaUi.clienteId())
-	                .orElseThrow(() ->
-	                        new ClienteNonTrovatoException(
-	                                utenteDaUi.clienteId()
-	                        )
+	                .orElseThrow(() ->{
+	                	logger.warn(
+	                            "Creazione utente CLIENTE fallita: clienteId={} non trovato",
+	                            utenteDaUi.clienteId());
+	                	throw new ClienteNonTrovatoException(utenteDaUi.clienteId());
+	                }
 	                );
 
 	        utente.setCliente(clienteDaDb);
+	        logger.debug(
+	                "Cliente id={} associato al nuovo utente",
+	                clienteDaDb.getId()
+	        );
 	        utente.setPassword(passwordEncoder.encode(utenteDaUi.password()));
 	        utente.setStatoUtente(StatoUtente.ATTIVO);
 	        utente.setDataCreazione(LocalDateTime.now());
 	    }
 
 	    Utente utenteSalvato = utenteRepository.save(utente);
+
+	    logger.info(
+	            "Utente creato correttamente id={} ruolo={}",
+	            utenteSalvato.getId(),
+	            utenteSalvato.getRuoloUtente()
+	    );
 
 	    return utenteMapper.utenteToUtenteResponseDto(utenteSalvato);
 	}
